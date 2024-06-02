87f927476eed8d1d9a6b480630d6727c1003c9f6
Aaron <agm.moya@alumno.um.edu.ar>
2024-06-02 14:08:55 -0300

    all service's costs formatted

--- src/utils/extract_utils/convert_data.py
+++ src/utils/extract_utils/convert_data.py
@@ -19,16 +19,20 @@ period_regex = [
     re.compile(r"Período Facturado:\s(\w+ \d{4})"),
 ]
 cost_regex = [
-    # re.compile(r"\$([0-9]+.[0-9]+)"),
+    re.compile(r"TOTAL \$ ([0-9]+,[0-9]+)"),
+    re.compile(r"TOTAL \$ ([0-9]+.[0-9]+,[0-9]+)"),
     re.compile(r"Costo:\s?(\$[0-9]+.[0-9]+)\s?Ref:"),
     re.compile(r"IMPORTE A PAGAR\s+\$?\s?(.*?)\s+DOMICILIO POSTAL"),
     re.compile(r"TOTAL A PAGAR \$\s(\d+.\d{2})"),
     re.compile(r"TOTAL \$\s(\d+.\d{2})"),
-    re.compile(r"\$(\d+.\d{2})"),
     re.compile(r"Total a pagar hasta el \d{2}/\d{2}/\d{4} \$ (\d+.\d{2})"),
+    re.compile(r"Tipo Lectura: Real\n\$ ([0-9]+.[0-9]+,[0-9]+)"),
+    # re.compile(r"\$([0-9]+.[0-9]+)"),
+    re.compile(r"\$(\d+.\d{2})"),
 ]
 consumption_regex = [
     re.compile(r"Consumo Medido (\d+) m³"),
+    re.compile(r"Cargo Variable kWh (\d+)"),
 ]
 
 
@@ -86,5 +90,9 @@ async def convert_data_to_json(data):
         "address": address.group(1) if address else None,
         "consumption": consumption.group(1) if consumption else None,
         "period": period.group(1) if period else None,
-        "cost": cost.group(1) if cost else None,
+        "cost": (
+            "{:.2f}".format(float(cost.group(1).replace(".", "").replace(",", ".")))
+            if cost
+            else None
+        ),
     }
